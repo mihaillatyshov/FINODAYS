bc1754e19e8e8627cd03e7cbdda01151a95931d9
danielgof <gofmandon@gmail.com>
2021-09-16 00:33:30 +0300

    neural network

--- neural/test.py
+++ neural/test.py
@@ -20,6 +20,9 @@ from sklearn.metrics import plot_precision_recall_curve
 import numpy as np
 from sklearn.model_selection import GridSearchCV
 
+
+
+
 df = pd.read_csv("testdata.manual.2009.06.14.csv", sep=",")
 
 df.head()
@@ -34,27 +37,27 @@ df.describe
 
 df_new = df.drop([1, 2, 3, 4], axis=1)
 
-df_new
+df = df_new
 
-df[0].value_counts()
+df["a"].value_counts()
 
-for c in df[df[0] == 4][5].head(5):
+for c in df[df["a"] == 0][5].head(5):
     print(c)
 
-for c in df[df[0] == 2][5].head(5):
+for c in df[df["a"] == 2][5].head(5):
     print(c)
 
-for c in df[df[0] == 0][5].head(5):
+for c in df[df["a"] == 4][5].head(5):
     print(c)
 
-train_df, test_df = train_test_split(df, test_size=20)
+train_df, test_df = train_test_split(df, test_size=80)
 
 sentence_example = df.iloc[1][5]
-tokens = word_tokenize(sentence_example, language="english")
+tokens = word_tokenize(sentence_example, language="russian")
 tokens_without_punctuation = [i for i in tokens if i not in string.punctuation]
-russian_stop_words = stopwords.words("english")
+russian_stop_words = stopwords.words("russian")
 tokens_without_stop_words_and_punctuation = [i for i in tokens_without_punctuation if i not in russian_stop_words]
-snowball = SnowballStemmer(language="english")
+snowball = SnowballStemmer(language="russian")
 stemmed_tokens = [snowball.stem(i) for i in tokens_without_stop_words_and_punctuation]
 
 print(f"Исходный текст: {sentence_example}")
@@ -68,14 +71,14 @@ print("-----------------")
 print(f"Токены после стемминга: {stemmed_tokens}")
 print("-----------------")
 
-snowball = SnowballStemmer(language="english")
-english_stop_words = stopwords.words("english")
+snowball = SnowballStemmer(language="russian")
+russian_stop_words = stopwords.words("russian")
 
 def tokenize_sentence(sentence: str, remove_stop_words: bool = True):
-    tokens = word_tokenize(sentence, language="english")
+    tokens = word_tokenize(sentence, language="russian")
     tokens = [i for i in tokens if i not in string.punctuation]
     if remove_stop_words:
-        tokens = [i for i in tokens if i not in english_stop_words]
+        tokens = [i for i in tokens if i not in russian_stop_words]
     tokens = [snowball.stem(i) for i in tokens]
     return tokens
 
@@ -86,11 +89,11 @@ vectorizer = TfidfVectorizer(tokenizer=lambda x: tokenize_sentence(x, remove_sto
 features = vectorizer.fit_transform(train_df[5])
 
 model = LogisticRegression(random_state=0)
-model.fit(features, train_df[0])
+model.fit(features, train_df["a"])
 
-model.predict(features[0])
+model.predict(features["a"])
 
-train_df[5].iloc[0]
+train_df[5].iloc["a"]
 
 model_pipeline = Pipeline([
     ("vectorizer", TfidfVectorizer(tokenizer=lambda x: tokenize_sentence(x, remove_stop_words=True))),
@@ -98,7 +101,7 @@ model_pipeline = Pipeline([
 ]
 )
 
-model_pipeline.fit(train_df[5], train_df[0])
+model_pipeline.fit(train_df[5], train_df["a"])
 
 model_pipeline.predict(["Hello, are you okay?"])
 
