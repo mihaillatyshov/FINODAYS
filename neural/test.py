c596c7d97d05b0591466dfe71945f9ba0d8ea32f
danielgof <gofmandon@gmail.com>
2021-09-16 22:11:53 +0300

    neural network changes

--- neural/test.py
+++ neural/test.py
@@ -1,7 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# -- Sheet --
-
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import nltk
@@ -41,44 +37,44 @@ df = df_new
 
 df["a"].value_counts()
 
-for c in df[df["a"] == 0][5].head(5):
-    print(c)
+# for c in df[df["a"] == 0][5].head(5):
+#     print(c)
 
-for c in df[df["a"] == 2][5].head(5):
-    print(c)
+# for c in df[df["a"] == 2][5].head(5):
+#     print(c)
 
-for c in df[df["a"] == 4][5].head(5):
-    print(c)
+# for c in df[df["a"] == 4][5].head(5):
+#     print(c)
 
 train_df, test_df = train_test_split(df, test_size=80)
 
 sentence_example = df.iloc[1][5]
-tokens = word_tokenize(sentence_example, language="russian")
+tokens = word_tokenize(sentence_example, language="english")
 tokens_without_punctuation = [i for i in tokens if i not in string.punctuation]
-russian_stop_words = stopwords.words("russian")
-tokens_without_stop_words_and_punctuation = [i for i in tokens_without_punctuation if i not in russian_stop_words]
-snowball = SnowballStemmer(language="russian")
+english_stop_words = stopwords.words("english")
+tokens_without_stop_words_and_punctuation = [i for i in tokens_without_punctuation if i not in english_stop_words]
+snowball = SnowballStemmer(language="english")
 stemmed_tokens = [snowball.stem(i) for i in tokens_without_stop_words_and_punctuation]
 
-print(f"Исходный текст: {sentence_example}")
-print("-----------------")
-print(f"Токены: {tokens}")
-print("-----------------")
-print(f"Токены без пунктуации: {tokens_without_punctuation}")
-print("-----------------")
-print(f"Токены без пунктуации и стоп слов: {tokens_without_stop_words_and_punctuation}")
-print("-----------------")
-print(f"Токены после стемминга: {stemmed_tokens}")
-print("-----------------")
+# print(f"Исходный текст: {sentence_example}")
+# print("-----------------")
+# print(f"Токены: {tokens}")
+# print("-----------------")
+# print(f"Токены без пунктуации: {tokens_without_punctuation}")
+# print("-----------------")
+# print(f"Токены без пунктуации и стоп слов: {tokens_without_stop_words_and_punctuation}")
+# print("-----------------")
+# print(f"Токены после стемминга: {stemmed_tokens}")
+# print("-----------------")
 
-snowball = SnowballStemmer(language="russian")
-russian_stop_words = stopwords.words("russian")
+snowball = SnowballStemmer(language="english")
+english_stop_words = stopwords.words("english")
 
 def tokenize_sentence(sentence: str, remove_stop_words: bool = True):
-    tokens = word_tokenize(sentence, language="russian")
+    tokens = word_tokenize(sentence, language="english")
     tokens = [i for i in tokens if i not in string.punctuation]
     if remove_stop_words:
-        tokens = [i for i in tokens if i not in russian_stop_words]
+        tokens = [i for i in tokens if i not in english_stop_words]
     tokens = [snowball.stem(i) for i in tokens]
     return tokens
 
